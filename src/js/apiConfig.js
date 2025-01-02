2e6020ad689df28b98aefaeb6a920f3fc14afb38
Umang Rathod <137269426+umangrathod11@users.noreply.github.com>
2025-01-02 19:03:47 +0530

    Update apiConfig.js

--- src/js/apiConfig.js
+++ src/js/apiConfig.js
@@ -1,9 +1,9 @@
 const hostname = window.location.hostname;
 
 let baseUrl;
-if (hostname === 'business.fggroup.in') {
+if (hostname === 'gyms.fggroup.in') {
   baseUrl = 'https://api.fggroup.in';
-} else if (hostname === 'test-business.fggroup.in') {
+} else if (hostname === 'test-gyms.fggroup.in') {
   baseUrl = 'https://dev-api.fggroup.in';
 } else {
   baseUrl = 'https://dev-api.fggroup.in';
